9e2dbe1651d85c42e5f76ea8c6efa45c96c336eb
temaari <53286793+temaari@users.noreply.github.com>
2022-05-07 18:06:09 +1200

    use params instead of query and change put to patch

--- models/BasicUser.js
+++ models/BasicUser.js
@@ -2,11 +2,25 @@ const mongoose = require('mongoose');
 
 // Schema
 const Schema = mongoose.Schema;
+const user = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    hobbies: {
+        type: String,
+        required: false
+    },
+    date_of_birth: {
+        type: Date,
+        required: true
+    }
+})
+
 
 // Model
-module.exports = mongoose.model('users', new Schema({
-    name: String,
-    age: Number,
-    hobbies: String,
-    date_of_birth: Date
-}))
+module.exports = mongoose.model('users', user)
--- routes/users.js
+++ routes/users.js
@@ -1,47 +1,66 @@
 const express = require('express')
 const router = express.Router()
-const USERS = require('../models/BasicUser')
+const BasicUser = require('../models/BasicUser')
 
 router
     .get('/', async (req, res) => {
         try {
-            const users = await findFirstOrDefault(req.query.id)
+            const users = await BasicUser.find({})
             res.json(users)
         } catch (error) {
-            res.redirect('/')
+            res.status(400).json({ message: error.message })
         }
     })
-    .post('/create', (req, res) => {
-        let data = req.body
-        if (!data) return res.status(400).json({ message: `Error! No new user. ${data}` })
-
-        let user = new USERS(data)
-        user.save(error => {
-            if (data && !error) return res.json({ message: 'You have successfully added a new user' })
-
-            return res.status(500).json({ message: `Error! Internal server error. ${error}` })
-        })
-    })
-    .put('/',  async (req, res) => {
+    .get('/:id', findUser, async (req, res) => {
         try {
-            let user = await USERS.findByIdAndUpdate(req.query.id, req.body)
-            res.json({ message: `update`, user })
+            res.json(res.user)
         } catch (error) {
-            res.status(500).json({ message: `Error! ${error}` })
+            res.status(400).json({ message: error.message })
         }
     })
-    .delete('/', async (req, res) => {
+    .patch('/:id',  findUser, async (req, res) => {
+        if (req.body.name) res.user.name = req.body.name
+        if (req.body.age) res.user.age = req.body.age
+        if (req.body.hobbies) res.user.hobbies = req.body.hobbies
+        if (req.body.date_of_birth) res.user.date_of_birth = req.body.date_of_birth
+
         try {
-            await USERS.findByIdAndDelete(req.query.id)
+            await res.user.save()
+            res.json({ message: `user updated` })
+        } catch (error) {
+            res.status(400).json({ message: error.message })
+        }
+    })
+    .delete('/:id', findUser, async (req, res) => {
+        try {
+            await res.user.remove()
             res.json({ message: 'User deleted' })
         } catch (error) {
-            res.status(500).json({ message: `Error! could not remove user ${req.query.id}. ${error}` })
+            res.status(500).json({ message: error.message })
+        }
+    })
+    .post('/create', async (req, res) => {
+        try {
+            let user = new BasicUser(req.body)
+            await user.save()
+            res.json({ message: 'You have successfully created a new user' })
+        } catch (error) {
+            return res.status(400).json({ message: error.message })
         }
     })
 
-async function findFirstOrDefault(id) {
-    if (id) return await USERS.findById(id) 
-    return await USERS.find({})
+async function findUser(req, res, next) {
+    let user
+
+    try {
+        user = await BasicUser.findById(req.params.id) 
+        if (!user) return res.status(404).json({ message: "cannot find user"})
+    } catch (error) {
+        return res.status(500).json({ message: `Internal server error. ${error.message}` })
+    }
+
+    res.user = user
+    next()
 }
 
 module.exports = router
